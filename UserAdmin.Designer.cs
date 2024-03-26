bc367085e323352393d26d5b4fc52cc4db629912
Nicopug03 <nicolaspuga2000@gmail.com>
2024-03-25 22:34:53 -0300

    Cambio de Names en Labels y Forms

--- UserAdmin.Designer.cs
+++ UserAdmin.Designer.cs
@@ -33,7 +33,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.label2 = new System.Windows.Forms.Label();
+            this.LBLNombre = new System.Windows.Forms.Label();
+            this.LBLApellido = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -62,7 +64,7 @@
             this.label1.Location = new System.Drawing.Point(34, 35);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Prueba";
             this.label1.Click += new System.EventHandler(this.Label1_Click);
@@ -70,35 +72,53 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(390, 141);
+            this.comboBox1.Location = new System.Drawing.Point(52, 85);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(140, 23);
+            this.comboBox1.Size = new System.Drawing.Size(140, 21);
             this.comboBox1.TabIndex = 3;
             // 
-            // label2
+            // LBLNombre
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(240, 218);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.LBLNombre.AutoSize = true;
+            this.LBLNombre.Location = new System.Drawing.Point(248, 114);
+            this.LBLNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBLNombre.Name = "LBLNombre";
+            this.LBLNombre.Size = new System.Drawing.Size(44, 13);
+            this.LBLNombre.TabIndex = 4;
+            this.LBLNombre.Text = "Nombre";
             // 
-            // Form1
+            // LBLApellido
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.LBLApellido.AutoSize = true;
+            this.LBLApellido.Location = new System.Drawing.Point(248, 141);
+            this.LBLApellido.Name = "LBLApellido";
+            this.LBLApellido.Size = new System.Drawing.Size(44, 13);
+            this.LBLApellido.TabIndex = 5;
+            this.LBLApellido.Text = "Apellido";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(409, 189);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 6;
+            // 
+            // UserAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1107, 661);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.LBLApellido);
+            this.Controls.Add(this.LBLNombre);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Font = new System.Drawing.Font("Nunito", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.Name = "Form1";
+            this.Name = "UserAdmin";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -112,7 +132,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ColorDialog colorDialog1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LBLNombre;
+        private System.Windows.Forms.Label LBLApellido;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
     }
 }
 
