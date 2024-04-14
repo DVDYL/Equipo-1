ac8bd5df61a84d0dc5e1f5bc64c4de1d788a9d26
tatosuv <lisandroasuvercase@gmail.com>
2024-04-14 20:01:34 -0300

    Revert "Se establece URL base para WS."
    This reverts commit bfc067dcf7da0f2dc46555a3a7b34aa7fdf91f1b.

--- Persistencia/WebService.cs
+++ Persistencia/WebService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http; //Se importa código para WS.
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +8,5 @@ namespace Persistencia
 {
     public class WebService
     {
-        private readonly HttpClient _client;  // Declaramos un campo privado de tipo HttpClient llamado _client
-
-        public WebService() // Constructor de la clase WebService
-        {
-            _client = new HttpClient(); // Inicializamos el campo _client con un nuevo objeto HttpClient
-            _client.BaseAddress = new Uri("https://cai-tp.azurewebsites.net/api/"); // Establecemos URL base del WS donde se va a conectar el cliente HttpClient
-        }
-
-        // De acá en adelante, pondremos los métodos correspondientes, luego de la explicación del profesor.
-        //Una vez que tengamos los métodos correspondientes, los llamaremos en las instancias que correspondan
     }
 }
