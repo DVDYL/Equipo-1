e2875c79ba5bdd7b1ca89e622ff614989c1412d8
Nicopug03 <nicolaspuga2000@gmail.com>
2024-04-21 15:41:42 -0300

    Se agrega Service de Proveedor (Metodo DELETE no tiene parametro como si lo tiene cliente, deberiamos validar eso)

---
+++ Datos/AltaProveedor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class AltaProveedor
+    {
+        String _idUsuario;
+        String _nombre;
+        String _apellido;
+        String _email;
+        String _cuit;
+
+        public AltaProveedor(string idUsuario, string nombre, string apellido, string email, string cuit)
+        {
+            _idUsuario = idUsuario;
+            _nombre = nombre;
+            _apellido = apellido;
+            _email = email;
+            _cuit = cuit;
+        }
+
+        public string IdUsuario { get => _idUsuario; set => _idUsuario = value; }
+
+        public string Nombre { get => _nombre; set => _nombre = value; }
+
+        public string Apellido { get => _apellido; set => _apellido = value; }
+
+        public string Email { get => _email; set => _email = value; }
+
+        public string Cuit { get => _cuit; set => _cuit = value; }
+
+    }
+
+   
+  
+}
---
+++ Persistencia/ProveedorService.cs
@@ -0,0 +1,91 @@
+﻿using Datos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class ProveedorService
+    {
+        public List<Proveedores> GetProveedores()
+        {
+            String path = "/api/Proveedor/TraerProveedores";
+            List<Proveedores> Proveedores = new List<Proveedores>();
+            try
+            {
+                HttpResponseMessage response = WebHelper.Get(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    var contentStream = response.Content.ReadAsStringAsync().Result;
+                    List<Proveedores> listadoProveedores = JsonConvert.DeserializeObject<List<Proveedores>>(contentStream);
+                    return listadoProveedores;
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+            return Proveedores;
+        }
+
+        public void AgregarProveedor(AltaProveedor altaProveedor)
+        {
+            String path = "/api/Proveedor/AgregarProveedor";
+
+            var jsonRequest = JsonConvert.SerializeObject(altaProveedor);
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Post(path, jsonRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+        public void BorrarProveedor(Guid idProveedor) //Validar Tema de Parametro
+        {
+            String path = "/api/Proveedor/BajaProveedor" + idProveedor;
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Delete(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+    }
+}
