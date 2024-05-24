5d042962efbe9d1db1dc8a5a5e8545cd4441d762
Nicopug03 <nicolaspuga2000@gmail.com>
2024-05-23 23:28:29 -0300

    Se sube Venta Service y Venta Negocio (Quedan pendientes metodos de traer ventas y ventas by cliente)

---
+++ Negocio/VentaNegocio.cs
@@ -0,0 +1,32 @@
+﻿using Datos;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class VentaNegocio
+    {
+        private VentaService ventaService = new VentaService();
+        private String id = "70b37dc1-8fde-4840-be47-9ababd0ee7e5";
+
+        public void agregarVenta(string idCliente, string idUsuario, string idProducto, int cantidad)
+        {
+            AgregarVenta agregarVenta = new AgregarVenta(idCliente, idUsuario, idProducto, cantidad);
+            ventaService.AgregarVenta(agregarVenta);
+
+        }
+
+        public void devolverVenta(string idVenta, string idUsuario)
+        {
+            ventaService.DevolverVenta(idVenta,idUsuario);
+        }
+
+        //Faltan metodos de TraerVentas
+
+    }
+
+}
---
+++ Persistencia/VentaService.cs
@@ -0,0 +1,73 @@
+﻿using Datos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class VentaService
+    {
+        public void DevolverVenta(string idVenta, string idUsuario)
+        {
+            String path = "/api/Venta/DevolverVenta";
+            Dictionary<string, string> map = new Dictionary<string, string>();
+            map.Add("id", idVenta);
+            map.Add("idUsuario", idUsuario);
+
+            var jsonRequest = JsonConvert.SerializeObject(map);
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Patch(path, jsonRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+        public void AgregarVenta(AgregarVenta agregarVenta)
+        {
+            String path = "/api/Venta/AgregarVenta";
+
+            var jsonRequest = JsonConvert.SerializeObject(agregarVenta);
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Post(path, jsonRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+
+        }
+
+        // Faltan metodos para traer ventas
+    }
+}
